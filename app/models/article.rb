d1899a75e76f99e9d487ea8f5d20853671c54012
Adrian Grzywaczewski <1204969@rgu.ac.uk>
2020-03-10 21:07:03 +0100

    Simple Tests added for the Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { in: 5..100} 
+  validates :body, presence: true, length: { minimum: 5 } 
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save article without title" do
+    article = Article.new
+    assert_not article.save
+  end
+
+  test "should save article when body and title is present" do
+    article = Article.new(title: "title", body: "Valid body")
+    assert article.save
+  end
+
+  test "should not save article with too long title" do
+    too_long_title = 'title' * 255
+    article = Article.new(title: too_long_title, body: "Valid body")
+    assert_not article.save
+  end
 end
